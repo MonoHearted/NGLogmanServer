9c8c22e6c0190f3ca751f13160e47edb69a99e2c
Bryan Niu <bryan.niu@genesys.com>
2019-04-26 12:08:58 -0400

    Changed sample path to reflect current location

--- nglogman/forms.py
+++ nglogman/forms.py
@@ -6,7 +6,7 @@ from .models import NodeGroup
 class ConfigForm(forms.Form):
     file = forms.FileField(label='Config File (.ini)',
             help_text=mark_safe('Sample configuration can be found '
-                      r'<a href="/nodeConfigs/logman_sample.ini">here</a>.'))
+                      r'<a href="/statics/logman_sample.ini">here</a>.'))
 
     nodes = forms.ModelMultipleChoiceField(
             queryset=NodeGroup.objects.all(),
